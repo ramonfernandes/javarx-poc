d0c3ac3cb8f816b9a814553d268c46c6ed2dd303
ramon <ramon.correa@thomsonreuters.com>
2020-08-23 21:53:17 -0300

    Even more simple

--- src/main/java/GettingSimple.java
+++ src/main/java/GettingSimple.java
@@ -1,23 +1,15 @@
 import rx.Observable;
-import rx.functions.Action1;
 
 public class GettingSimple {
-
-  Observable<Integer> mySimpleObservable;
-  Action1<Integer> onNextAction = new Action1<Integer>() {
-    @Override
-    public void call(Integer value) {
-      System.out.println("Are ya winning son?");
-      if (value % 2 == 0) {
-        System.out.println("Nah, dad");
-      } else {
-        System.out.println("Perhaps");
-      }
-    }
-  };
-
   public GettingSimple() {
-    mySimpleObservable = Observable.range(0, 10);
-    mySimpleObservable.subscribe(onNextAction);
+    Observable.range(0, 10)
+        .subscribe(value -> {
+          System.out.println("Are ya winning son?");
+          if (value % 2 == 0) {
+            System.out.println("Nah, dad");
+          } else {
+            System.out.println("Perhaps");
+          }
+        });
   }
 }
